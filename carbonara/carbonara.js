81153f9637cdcf51e4e1c8b04f3200c4d56ed3c0
RennVa <0100L@protonmail.com>
2023-01-25 16:38:48 +0100

    first commit

---
+++ carbonara/carbonara.js
@@ -0,0 +1,16 @@
+const guanciale = 250;
+const yolks = 6*0.5;
+const parmesan = 50;
+const pepper = 4;
+const pasta = 400;
+
+let bowl = yolks + parmesan;
+let pan = pepper;
+
+pan = -pepper + guanciale;
+bowl += pepper;
+
+pan += pasta;
+
+let carbonara = pan + bowl;
+console.log(carbonara)
